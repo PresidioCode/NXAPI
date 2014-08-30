230c3328553196adab2a5ab8157c626b6e28ee02
James Caesar <jcaesar@jcaesar-MBP.local>
2014-08-30 13:17:00 -0400

    Updated RouteStats script
    Updated to include additional detail in the output.

--- RouteStats.py
+++ RouteStats.py
@@ -11,38 +11,74 @@
 
 from argparse import ArgumentParser
 from nxapi_base import NXOS
-from ipaddress import ip_address
+from ipaddress import ip_address, ip_network
+from requests import Timeout
 
 
-def RouteStats(routetable, vrf="default", addrf="ipv4"):
+def GetRouteStats(routetable, vrf="default", addrf="ipv4"):
     # TODO:  Need to update to handle multiple VRFs and address families
-    nexthop_stats = {}
+    nh_stats = {}
+    connected_types = ['direct', 'local', 'hsrp', 'vrrp', 'glbp']
     current_vrf = routetable['TABLE_vrf']['ROW_vrf']
     current_addrf = current_vrf['TABLE_addrf']['ROW_addrf']
     routes = current_addrf['TABLE_prefix']['ROW_prefix']
-    for route in routes:
-        nexthop_list = route['TABLE_path']['ROW_path']
+    for prefix in routes:
+        # TODO: Multiple subnets on interface will overwrite previous
+        nexthop_list = prefix['TABLE_path']['ROW_path']
         if type(nexthop_list) == dict:      #Only one next-hop
             nexthop_list = [nexthop_list]
         for nexthop in nexthop_list:
+            intf = nexthop['ifname']
+            subnet = ip_network(prefix['ipprefix'])
             nh_addr = ip_address(nexthop['ipnexthop'])
-            if nh_addr in nexthop_stats:
-                nexthop_stats[nh_addr] += 1
+            client = nexthop['clientname']
+            if intf not in nh_stats:
+                nh_stats[intf] = {}
+            if client in connected_types:    
+                if client == 'direct':
+                    nh_stats[intf][client] = subnet
+                else:
+                    nh_stats[intf][client] = nh_addr
             else:
-                nexthop_stats[nh_addr] = 1
-    return nexthop_stats
+                if 'hops' in nh_stats[intf]:
+                    if nh_addr in nh_stats[intf]['hops']:
+                        if client in nh_stats[intf]['hops'][nh_addr]:
+                            nh_stats[intf]['hops'][nh_addr][client] += 1
+                        else:
+                            nh_stats[intf]['hops'][nh_addr][client] = 1
+                    else:
+                        nh_stats[intf]['hops'][nh_addr] = {client : 1}
+                else:
+                    nh_stats[intf]['hops'] = {nh_addr : {client : 1}}
+    return nh_stats
+
+
+def PrintRouteStats(stats):
+    print ('-'*30)
+    for intf in sorted(stats.keys()):
+        print "{} (IP: {}, Net: {}):".format(intf, stats[intf]['local'], 
+            stats[intf]['direct'])
+        if 'hops' in stats[intf].keys():
+            for hop in stats[intf]['hops']:
+                for client in stats[intf]['hops'][hop]:
+                    print " " * 4 + "Nexthop: {}\tNum Routes: {} ({})".format(str(hop), 
+                        stats[intf]['hops'][hop][client], client)
 
 
 def main(args):
     n9k = NXOS(args.mgmt_ip)
     n9k.PromptCreds()
     print "Gathering data."
-    route_data = n9k.cli_show('show ip route')
-    stats = RouteStats(route_data)
-    for hop in sorted(stats.keys()):
-        print "NextHop: " + str(hop) + "\t\tNum Routes: " + str(stats[hop])
-
-
+    try:
+        route_data = n9k.cli_show('show ip route')
+        stats = GetRouteStats(route_data)
+        PrintRouteStats(stats)
+    except Timeout, e:
+        print e
+    except ValueError, e:
+        print e
+    print ""
+    
 if __name__ == '__main__':
     parser = ArgumentParser(description = "Enter the IP address of the device to connect with.")
     parser.add_argument("mgmt_ip", help="The management IP address of the NXAPI device you are "
